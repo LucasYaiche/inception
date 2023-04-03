c37682d0a1ad1d9d1cfc57cc309ee7c7d1c028ac
Lucas Yaïche <lucasyaiche@MacBook-Pro-de-Lucas.local>
2023-04-03 17:13:41 +0200

    correction

--- srcs/requirements/mariadb/conf/create_db.sql
+++ srcs/requirements/mariadb/conf/create_db.sql
@@ -3,6 +3,6 @@ CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PWD';
 GRANT ALL PRIVILEGES ON $MARIADB_DB.* TO $MARIADB_USER@'%';
 
 /* Clears the priviliege cache on the MariaDB database for the changes to take effect*/
-FLUSH PRIVILEGES
+FLUSH PRIVILEGES;
 
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PWD');
